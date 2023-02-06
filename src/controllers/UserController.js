5433c4c6d9b27804002bf54ab4d4efa515ab073a
Matheus <matheus-rodrigues37@live.com>
2023-02-06 01:23:45 -0300

    update UserController.js

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,9 +2,6 @@ class UserController {
 	async create(req, res) {
 		return res.send("Voce atingiu a rota user");
 	}
-	async update(req, res) {
-		return res.send("Voce atingiu a rota user");
-	}
 }
 
 module.exports = new UserController();
