13768c128d3252697d8476a6c483f784d5ff46fd
Matheus <matheus-rodrigues37@live.com>
2023-02-05 22:56:34 -0300

    config eslint prettier

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,23 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+const path = require("path");
+
 module.exports = {
   development: {
     client: "sqlite3",
     connection: {
       filename: "./dev.sqlite3",
     },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "knex", "migrations"),
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: path.resolve(__dirname, "src", "knex", "seeds"),
+    },
+    useNullAsDefault: true,
   },
   production: {
     client: "postgresql",
