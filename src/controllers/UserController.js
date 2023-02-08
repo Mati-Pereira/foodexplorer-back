1b506d5db7151961c08408ea683984bf50cb3c7c
Matheus <matheus-rodrigues37@live.com>
2023-02-08 17:49:46 -0300

    build(deps): update package.json, UserController.js, src/knex/index.js and yarn.lock

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,20 @@
+const { hash } = require("bcryptjs");
+const knex = require("../knex");
+const AppError = require("../utils/AppError");
+
 class UserController {
-	async create(req, res) {
-		return res.send("Voce atingiu a rota user");
+	async create(request, response) {
+		const { name, email, password } = request.body;
+		const checkUserExists = await knex("users").where({ email }).first();
+		if (checkUserExists) {
+			throw new AppError("E-mail já cadastrado, tente outro e-mail.");
+		}
+		const hashPassword = await hash(password, 8);
+		await knex("users").insert({ name, email, password: hashPassword });
+		return response.status(201).json({
+			status: "success",
+			message: "Usuário criado com sucesso!",
+		});
 	}
 }
 
