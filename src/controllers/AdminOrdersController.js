d514d6b9602ee4a6eb1fe96eb24d836cba4e3be0
Matheus <matheus-rodrigues37@live.com>
2023-02-09 16:32:05 -0300

    feat: create AdminOrdersController.js

---
+++ src/controllers/AdminOrdersController.js
@@ -0,0 +1,33 @@
+const knex = require("../knex");
+const AppError = require("../utils/AppError");
+
+class AdminOrdersController {
+	async show(req, res) {
+		const order = await knex("orders");
+		return res.json(order);
+	}
+	async update(req, res) {
+		const user_id = req.user.id;
+		const { id } = req.params;
+		const { status } = req.body;
+		const user = await knex("users").where({ id: user_id }).first();
+		if (user.is_admin) {
+			if (
+				status === "delivered" ||
+				status === "preparing" ||
+				status === "pending"
+			) {
+				await knex("orders")
+					.where({ id })
+					.update({ status, updated_at: knex.fn.now() });
+			} else {
+				throw new AppError("Status inválido", 400);
+			}
+		} else {
+			throw new AppError("Usuário não é administrador", 400);
+		}
+		return res.json({ message: "Pedido atualizado com sucesso" });
+	}
+}
+
+module.exports = new AdminOrdersController();
