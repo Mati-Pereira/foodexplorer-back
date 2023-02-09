b6c5b27abcc9b1500a4b58451f0d702a1c24a1e3
Matheus <matheus-rodrigues37@live.com>
2023-02-09 12:59:11 -0300

    up

---
+++ src/knex/migrations/20230209155755_create_products.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+	return knex.schema.createTable("products", (table) => {
+		table.increments("id").primary();
+		table.string("name").notNullable();
+		table.string("description").notNullable();
+		table.string("price").notNullable();
+		table.string("image").notNullable();
+		table.string("category").notNullable();
+	});
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+	return knex.schema.dropTableIfExists("products");
+};
