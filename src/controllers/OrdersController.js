ee9b36e684750841fab2e4a5b41569ff502adaf2
Matheus <matheus-rodrigues37@live.com>
2023-02-09 16:14:23 -0300

    feat: create OrdersController.js

---
+++ src/controllers/OrdersController.js
@@ -0,0 +1,29 @@
+const knex = require("../knex");
+const AppError = require("../utils/AppError");
+
+class OrdersController {
+	async index(req, res) {
+		const user_id = req.user.id;
+		const orders = await knex("orders").where({ user_id });
+		return res.json(orders);
+	}
+	async create(req, res) {
+		const user_id = req.user.id;
+
+		const { status, description } = req.body;
+
+		if (!status || !description) {
+			throw new AppError("Preencha todos os campos", 400);
+		}
+
+		await knex("orders").insert({
+			status,
+			description,
+			user_id,
+		});
+
+		return res.json({ message: "Pedido criado com sucesso" });
+	}
+}
+
+module.exports = new OrdersController();
