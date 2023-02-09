c4d0138c76777cf7aecd26c8b1077c689ea2dd8e
Matheus <matheus-rodrigues37@live.com>
2023-02-09 16:53:13 -0300

    feat: create products.routes.js

---
+++ src/routes/products.routes.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
+
+const upload = multer(uploadConfig.MULTER);
+const productsRoutes = Router();
+const productsController = require("../controllers/ProductsController");
+
+productsRoutes.get("/", productsController.index);
+productsRoutes.get("/:id", productsController.show);
+productsRoutes.post("/", upload.single("image"), productsController.create);
+productsRoutes.put(
+	"/:id",
+	upload.single("image"),
+	productsController.attributes,
+);
+productsRoutes.delete("/:id", productsController.delete);
+
+module.exports = productsRoutes;
