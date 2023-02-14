5b895c4925e6c9c0cf40485fb84217fed4cae62f
Matheus <matheus-rodrigues37@live.com>
2023-02-14 10:08:06 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -9,11 +9,11 @@ const routes = require("./routes");
 const app = express();
 
 app.use(cors());
-app.options("*", cors());
 app.use(express.json());
 app.use(routes);
 app.use("/", express.static(uploadConfig.UPLOADS_FOLDER));
 
+// eslint-disable-next-line no-unused-vars
 app.use((error, request, response, next) => {
 	if (error instanceof AppError) {
 		return response.status(error.statusCode).json({
