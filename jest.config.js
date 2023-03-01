33ea23812c3f325496c71000fddc458dc36d2554
Matheus <matheus-rodrigues37@live.com>
2023-03-01 17:05:43 -0300

    delete 2 files and update 1 file

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	testEnvironment: "node",
-	testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"],
-	testPathIgnorePatterns: ["/node_modules/"],
-	moduleFileExtensions: ["js", "node", "json"],
-};
