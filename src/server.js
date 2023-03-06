3ae2bcff742a3f3ffaa269e169d84f8c72434f4d
Matheus <matheus-rodrigues37@live.com>
2023-03-06 16:33:34 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -8,8 +8,8 @@ const routes = require("./routes");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.use("/", express.static(uploadConfig.UPLOADS_FOLDER));
 
