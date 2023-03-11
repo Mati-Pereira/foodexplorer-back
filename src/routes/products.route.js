f0f6101188c14cdd3003061bb767682cb3b8203c
Matheus <matheus-rodrigues37@live.com>
2023-03-11 16:48:06 -0300

    update products.route.js

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,12 +1,15 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
+const upload = multer(uploadConfig.MULTER);
 const productsRoutes = Router();
 const productsController = require("../controllers/ProductsController");
 
 productsRoutes.get("/", productsController.index);
 productsRoutes.get("/:id", productsController.show);
-productsRoutes.post("/", productsController.create);
-productsRoutes.put("/:id", productsController.update);
+productsRoutes.post("/", upload.single("image"), productsController.create);
+productsRoutes.put("/:id", upload.single("image"), productsController.update);
 productsRoutes.delete("/:id", productsController.delete);
 
 module.exports = productsRoutes;
