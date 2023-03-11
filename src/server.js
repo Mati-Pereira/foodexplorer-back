9353567866f69ca34a89773d83911aa6b2372b4a
Matheus <matheus-rodrigues37@live.com>
2023-03-11 19:33:54 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -7,8 +7,16 @@ const routes = require("./routes");
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(
+	express.json({
+		limit: "25mb",
+	}),
+);
+app.use(express.urlencoded({ extended: true, limit: "25mb" }));
+app.use((_, res) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+});
 app.use(routes);
 
 // eslint-disable-next-line no-unused-vars
