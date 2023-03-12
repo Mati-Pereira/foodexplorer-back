cd1acf214caabe0bbbddd97e0bcfb208ce94510c
Matheus <matheus-rodrigues37@live.com>
2023-03-12 15:30:26 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,21 +1,19 @@
 require("dotenv/config");
 require("express-async-errors");
+const cors = require("cors");
 const AppError = require("./utils/AppError");
 const express = require("express");
-const cors = require("cors");
 const uploadConfig = require("./configs/upload");
 const routes = require("./routes");
 
 const app = express();
 
 app.use(cors());
-app.use(
-	express.json({
-		limit: "25mb",
-	}),
-);
-app.use(express.urlencoded({ extended: true, limit: "25mb" }));
+
+app.use(express.json());
+
 app.use("/", express.static(uploadConfig.UPLOAD_FOLDER));
+
 app.use(routes);
 
 // eslint-disable-next-line no-unused-vars
