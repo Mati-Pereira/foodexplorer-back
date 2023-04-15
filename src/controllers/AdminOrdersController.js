11d246d37c5351823dc54b23bc09ddde1c8923fb
Matheus <matheus-rodrigues37@live.com>
2023-04-15 15:19:22 -0300

    build(deps): update package-lock.json, package.json, AdminOrdersController.js and isAuthenticated.js

--- src/controllers/AdminOrdersController.js
+++ src/controllers/AdminOrdersController.js
@@ -3,38 +3,38 @@ const AppError = require("../utils/AppError");
 const moment = require("moment-timezone");
 
 class AdminOrdersController {
-	async show(req, res) {
-		const order = await knex("orders");
-		return res.json(order);
-	}
-	async update(req, res) {
-		const user_id = req.user.id;
-		const { id } = req.params;
-		const { status } = req.body;
-		const user = await knex("users").where({ id: user_id }).first();
-		const is_admin = Boolean(user.is_admin);
-		if (is_admin) {
-			if (
-				status === "delivered" ||
-				status === "preparing" ||
-				status === "pending"
-			) {
-				await knex("orders")
-					.where({ id })
-					.update({
-						status,
-						updated_at: moment()
-							.tz("America/Sao_Paulo")
-							.format("YYYY-MM-DD HH:mm:ss"),
-					});
-			} else {
-				throw new AppError("Status inválido", 400);
-			}
-		} else {
-			throw new AppError("Usuário não é administrador", 400);
-		}
-		return res.json({ message: "Pedido atualizado com sucesso" });
-	}
+  async show(req, res) {
+    const order = await knex("orders");
+    return res.json(order);
+  }
+  async update(req, res) {
+    const user_id = req.user.id;
+    const { id } = req.params;
+    const { status } = req.body;
+    const user = await knex("users").where({ id: user_id }).first();
+    const is_admin = Boolean(user.is_admin);
+    if (is_admin) {
+      if (
+        status === "delivered" ||
+        status === "preparing" ||
+        status === "pending"
+      ) {
+        await knex("orders")
+          .where({ id })
+          .update({
+            status,
+            updated_at: moment()
+              .tz("America/Sao_Paulo")
+              .format("YYYY-MM-DD HH:mm:ss"),
+          });
+      } else {
+        throw new AppError("Status inválido", 400);
+      }
+    } else {
+      throw new AppError("Usuário não é administrador", 400);
+    }
+    return res.json({ message: "Pedido atualizado com sucesso" });
+  }
 }
 
 module.exports = new AdminOrdersController();
--- src/middleware/isAuthenticated.js
+++ src/middleware/isAuthenticated.js
@@ -2,19 +2,19 @@ const { verify } = require("jsonwebtoken");
 const AppError = require("../utils/AppError");
 const authConfig = require("../configs/auth");
 function ensureAuthenticated(request, response, next) {
-	const authHeader = request.headers.authorization;
-	if (!authHeader) {
-		throw new AppError("JWT token inválido");
-	}
-	const [, access_token] = authHeader.split(" ");
-	try {
-		const { sub: user_id } = verify(access_token, authConfig.jwt.secret);
-		request.user = {
-			id: Number(user_id),
-		};
-		return next();
-	} catch (err) {
-		throw new AppError(err.message, 401);
-	}
+  const authHeader = request.headers.authorization;
+  if (!authHeader) {
+    throw new AppError("JWT token inválido");
+  }
+  const [, access_token] = authHeader.split(" ");
+  try {
+    const { sub: user_id } = verify(access_token, authConfig.jwt.secret);
+    request.user = {
+      id: Number(user_id),
+    };
+    return next();
+  } catch (err) {
+    throw new AppError(err.message, 401);
+  }
 }
 module.exports = ensureAuthenticated;
