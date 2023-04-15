a382c630738cb66001034c3ff258d3d06197b4a4
Matheus <matheus-rodrigues37@live.com>
2023-04-15 17:28:35 -0300

    update SessionController.js

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -7,12 +7,7 @@ const authConfig = require("../configs/auth");
 class SessionsController {
   async create(req, res) {
     const { email, password } = req.body;
-    const user = await knex("users")
-      .select("id", "username", "is_admin", "password")
-      .where({ email })
-      .first()
-      .innerJoin("user_roles", "users.id", "user_roles.user_id")
-      .where("user_roles.role_id", 1);
+    const user = await knex("users").where({ email }).first();
     if (!user) {
       throw new AppError("Email e/ou Senha estão inválidos", 401);
     }
