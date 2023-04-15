e36f4dc06a33aa4dac6b89d4132811c387aa7772
Matheus <matheus-rodrigues37@live.com>
2023-04-15 15:23:39 -0300

    update knexfile.js, package.json and AdminOrdersController.js

--- knexfile.js
+++ knexfile.js
@@ -6,14 +6,16 @@ const dotenv = require("dotenv");
 dotenv.config({ path: ".env" });
 
 module.exports = {
-	client: "pg",
-	connection: process.env.DATABASE_URL,
-	migrations: {
-		directory: path.resolve(__dirname, "src", "knex", "migrations"),
-		tableName: "knex_migrations",
-	},
-	seeds: {
-		directory: path.resolve(__dirname, "src", "knex", "seeds"),
-	},
-	useNullAsDefault: true,
+  client: "sqlite3",
+  connection: {
+    filename: path.resolve(__dirname, "src", "database", "database.db"),
+  },
+  migrations: {
+    directory: path.resolve(__dirname, "src", "knex", "migrations"),
+    tableName: "knex_migrations",
+  },
+  seeds: {
+    directory: path.resolve(__dirname, "src", "knex", "seeds"),
+  },
+  useNullAsDefault: true,
 };
--- src/controllers/AdminOrdersController.js
+++ src/controllers/AdminOrdersController.js
@@ -8,31 +8,27 @@ class AdminOrdersController {
     return res.json(order);
   }
   async update(req, res) {
-    const user_id = req.user.id;
-    const { id } = req.params;
-    const { status } = req.body;
-    const user = await knex("users").where({ id: user_id }).first();
-    const is_admin = Boolean(user.is_admin);
-    if (is_admin) {
-      if (
-        status === "delivered" ||
-        status === "preparing" ||
-        status === "pending"
-      ) {
-        await knex("orders")
-          .where({ id })
-          .update({
-            status,
-            updated_at: moment()
-              .tz("America/Sao_Paulo")
-              .format("YYYY-MM-DD HH:mm:ss"),
-          });
-      } else {
-        throw new AppError("Status inválido", 400);
-      }
-    } else {
+    const user = await knex("users").where({ id: req.user.id }).first();
+    if (!user.is_admin) {
       throw new AppError("Usuário não é administrador", 400);
     }
+    const { id } = req.params;
+    const { status } = req.body;
+    if (
+      status !== "delivered" &&
+      status !== "preparing" &&
+      status !== "pending"
+    ) {
+      throw new AppError("Status inválido", 400);
+    }
+    await knex("orders")
+      .where({ id })
+      .update({
+        status,
+        updated_at: moment()
+          .tz("America/Sao_Paulo")
+          .format("YYYY-MM-DD HH:mm:ss"),
+      });
     return res.json({ message: "Pedido atualizado com sucesso" });
   }
 }
