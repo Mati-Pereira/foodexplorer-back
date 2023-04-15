2db58f1383539d4984eb0648690c70a521152da2
Matheus <matheus-rodrigues37@live.com>
2023-04-15 16:09:30 -0300

    update .gitignore and knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ dotenv.config({ path: ".env" });
 module.exports = {
   client: "sqlite3",
   connection: {
-    filename: path.resolve(__dirname, "src", "database", "database.db"),
+    filename: path.resolve(__dirname, "src", "knex", "database.db"),
   },
   migrations: {
     directory: path.resolve(__dirname, "src", "knex", "migrations"),
