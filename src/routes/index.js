84abda7c6d0b55a0d8b7cf72d2a2a4468c6e1f84
Matheus <matheus-rodrigues37@live.com>
2023-05-01 21:33:00 -0300

    delete 3 files and update 1 file

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ const { Router } = require("express");
 const user = require("./users.route");
 const sessions = require("./sessions.route");
 const admin = require("./adm.orders.route");
-const favorites = require("./favorites.route");
 const orders = require("./orders.route");
 const products = require("./products.route");
 
@@ -12,7 +11,6 @@ const router = Router();
 router.use("/users", user);
 router.use("/sessions", sessions);
 router.use("/adminOrders", admin);
-router.use("/favorites", favorites);
 router.use("/orders", orders);
 router.use("/products", products);
 
